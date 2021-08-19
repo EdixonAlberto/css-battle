f4e76bd70ddb632813987d9d83c5176120850218
EdixonAlberto <edixonalbertto@gmail.com>
2021-08-19 00:04:16 -0400

    refactor gulpfile and create new files in /tools
    - clean dependencies whitout use

--- packages/css-battle-api/gulpfile.js
+++ packages/css-battle-api/gulpfile.js
@@ -1,57 +1,35 @@
+// GULP MODULES
 const { src, dest, task, series } = require('gulp')
-const ts = require('gulp-typescript')
+const { createProject } = require('gulp-typescript')
 const rimraf = require('gulp-rimraf')
 const prettier = require('gulp-prettier')
-const { createHeaderMessage } = require('./tools/createHeaderMessage')
-const gulpRimraf = require('gulp-rimraf')
-var browserify = require("browserify")
-var tsify = require("tsify")
-var source = require("vinyl-source-stream")
 
-// Task Format
+// OTHER MODULES
+const { createBundle } = require('./tools/createBundle')
+
+// TASK FORMAT
 function format(done) {
   src('src/**/.*ts').pipe(prettier()).pipe(dest('src'))
-
   done()
 }
 
-// Task Clean
+// TASK CLEAN
 function clean(done) {
-  src('./dist', { read: false }).pipe(rimraf({ force: true }))
-
+  src('dist', { read: false }).pipe(rimraf({ force: true }))
   done()
 }
 
-// Task Transpile
+// TASK TRANSPILE
 function transpile(done) {
-  const tsProject = ts.createProject('tsconfig.json')
-
+  const tsProject = createProject('tsconfig.json')
   const tsResult = tsProject.src().pipe(tsProject())
   tsResult.pipe(dest('dist'))
-
   done()
 }
 
-// Task Bundle
+// TASK BUNDLE
 function bundle(done) {
-  const pathBundle = 'dist/bundle'
-  const pathTemplate = 'tools/templateBundle.js'
-  const headerMessage = createHeaderMessage()
-
-  browserify(pathTemplate, {
-    basedir: '.',
-    debug: false,
-    allowEmpty: true
-  })
-  .plugin(tsify)
-  .bundle((err, beffer) => {
-    if (err) {
-      console.error(err)
-    }
-    return `${headerMessage}\n${beffer}`
-  })
-  .pipe(source(pathBundle))
-  .pipe(dest(pathBundle))
+  createBundle()
   done()
 }
 
@@ -60,4 +38,4 @@ function minify(done) {
   done()
 }
 
-task('build', series(format, bundle, clean, transpile, ))
+task('build', series(format, clean, transpile, bundle))
